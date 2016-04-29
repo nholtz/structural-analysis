50e3dff6b83e6527775c2da43606f7232e12de1d
Neal Holtz <neal.holtz@carleton.ca>
2016-04-29 15:42:36 -0400

    Added Kulak & Grondin 8.2 Example in slope deflection

--- slope-deflection/sdutil2.py
+++ slope-deflection/sdutil2.py
@@ -1,3 +1,5 @@
+from __future__ import division
+
 class EF(object):
     
     """Instances store the four End Forces for a segment, and allow
@@ -36,43 +38,18 @@ class EF(object):
         """Return end forces accessed by subscript 0 to 3."""
         return self.efs[ix]
 
-import sys
-
-def protect(x):
-    """Return a protected value of x that won't screw up in divisions.
-    If x is an int, try to convert it to something else.  This is meant
-    to be used with simpy, mostly."""
-
-    if type(x) is not int:      # if its not an int, leave it alone.
-        return x
-
-    sympy = sys.modules.get('sympy',None) # if sympy is loaded, try that
-    if sympy:
-        return sympy.Integer(x)
-
-    try:
-        gv = get_ipython().user_global_ns  # try to use ipython globals; not sure about this
-        return gv['Integer'](x)
-    except:
-        pass
-
-    return float(x)             # otherwise, convert it to float
-
 class FEF(object):
 
     @staticmethod
     def udl(L,w):
         """Return FEMs and shears due to a full-span UDL.  Distributed load intensity = w.
         Span = L"""
-        x12 = protect(12)
-        x2 = protect(2)
-        return EF(L,-w*L*L/x12,w*L*L/x12,w*L/x2,-w*L/x2)
+        return EF(L,-w*L*L/12,w*L*L/12,w*L/2,-w*L/2)
 
     @staticmethod
     def p(L,P,a):
         """Return FEMs and shears due to a single concentrated force, P, a distance, a,
         from the left end.  Span = L."""
-        L = protect(L)
         b = L - a
         m0 = -P*a*b*b/(L*L)
         m1 = P*a*a*b/(L*L)
@@ -90,7 +67,6 @@ def SD(L,EI,t0,t1,d01=0):
        d01 = relative lateral displacment end 0 to end 1.  
     SD sign convention: moments and rotation are +ive CW, 
     relative displacement d01 is +ive for CW rotation of chord."""
-    L = protect(L)
     m0 = (EI/L)*(4*t0 + 2*t1 - 6*d01/L)
     m1 = (EI/L)*(2*t0 + 4*t1 - 6*d01/L)
     v1 = -(m0 + m1)/L
