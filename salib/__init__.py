539fbf0c8fd37f2f70411a6a3e80198cf7e1d468
Neal Holtz <neal.holtz@carleton.ca>
2016-05-20 16:26:14 -0400

    Fixed classmethod handling in @extend()

--- salib/__init__.py
+++ salib/__init__.py
@@ -84,7 +84,10 @@ def extend(old):
         for a,v in inspect.getmembers(new):
             if not a.startswith('_') or a in ok:
                 if type(v) is types.MethodType:
-                    v = types.MethodType(v.im_func,old if v.im_self is new else v.im_self,old)
+                    if v.im_self is None:
+                        v =  types.MethodType(v.im_func,None,old)
+                    else:
+                        v = classmethod(v.im_func)
                 elif type(v) is property:
                     v = property(v.fget,v.fset,v.fdel)
                 elif type(v) is types.FunctionType:
