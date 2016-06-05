8cf21f6b3f7e5bc59ca854a59b3157bd057c75dc
Neal Holtz <neal.holtz@carleton.ca>
2016-06-05 12:17:39 -0400

    Ported P-Delta analysis from V03 to V04

--- matrix-methods/frame2d-v04/Frame2D_SolveFirstOrder.py
+++ matrix-methods/frame2d-v04/Frame2D_SolveFirstOrder.py
@@ -1,4 +1,4 @@
-## Compiled from Frame2D_SolveFirstOrder.py on Sat Jun  4 16:39:25 2016
+## Compiled from Frame2D_SolveFirstOrder.py on Sun Jun  5 12:12:41 2016
 
 ## In [1]:
 from __future__ import print_function
@@ -79,14 +79,33 @@ class Frame2D:
             if fefs is not None:
                 mefs += fefs
             all_efs[memb] = mefs
-        rs.member_efs = all_efs
-        return rs
+        return all_efs
 
 ## In [11]:
 @extend
 class Frame2D:
     
-    def solve(self,loadcase='all'):
+    def pdelta_forces(self,rs):
+        D = rs.node_displacements
+        pdf = np.mat(np.zeros((self.ndof,1)))
+        for m in self.columns:
+            ef = rs.member_efs[m].fefs
+            efg = m.Tm.T * ef    # end forces in global coords !!! TOO MUCH CALC !!!!
+            P = efg[1,0]
+            j = m.nodej.dofnums[0]
+            k = m.nodek.dofnums[0]
+            Delta = D[k,0]-D[j,0]
+            H = m.L
+            pdf[k,0] += P*Delta/H
+            pdf[j,0] -= P*Delta/H
+            ##print(m.id,H,j,k,P,Delta,P*Delta/H)
+        return pdf
+
+## In [12]:
+@extend
+class Frame2D:
+    
+    def solve(self,loadcase='all',pdelta=False,maxniter=10):
         
         self.number_dofs()
         K = self.buildK()
@@ -108,10 +127,34 @@ class Frame2D:
         D[:N] = np.linalg.solve(Kff,P[:N] - Kfc*D[N:])  # displacements
         R = Kcf*D[:N] + Kcc*D[N:] - P[N:]    # reactions at the constrained DOFs
         rs.node_displacements = D
-        rs.reaction_forces = R
+        rs.reaction_forces = R   
+        rs.member_efs = self.calculate_mefs(rs)
         
-        rs = self.calculate_mefs(rs=rs)
+        if pdelta:
+            niter = 0 
+            self.columns = [m for m in self.members.values() if abs(m.dcy) >= 0.95]
+            while True:
+                if niter > maxniter:
+                    raise Exception('Too many iterations.  Giving up.  Solution not usable.')
+                niter += 1
+                Dprev = D.copy()
+                
+                PDF = self.pdelta_forces(rs)
+                PDF[N:] = 0.
+        
+                D[:N] = np.linalg.solve(Kff,P[:N] + PDF[:N] - Kfc*D[N:])
+                rs.node_displacements = D
+                rs.member_efs = self.calculate_mefs(rs)
+                
+                maxD = max(abs(D[:N]))[0,0]
+                maxChange = max(abs(D[:N]-Dprev[:N]))[0,0]
+                maxP = 100.*maxChange/maxD
+                print('iter={}, max D={}, max chg={}, max % chg={}'.format(niter,maxD,maxChange,maxP))
+                if maxP < .01:
+                    break
+            R = Kcf*D[:N] + Kcc*D[N:] - P[N:] # reactions at the constrained DOFs (exclude P-delta forces)
+            rs.reaction_forces = R
+            rs.pdelta_forces = PDF
+            print()
+            
         return rs
-
-## In [ ]:
-
