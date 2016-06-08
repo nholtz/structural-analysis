91b5482246c53b9b7a9c7d6cac6e5a95e93a5e58
Neal Holtz <neal.holtz@carleton.ca>
2016-06-08 13:29:29 -0400

    V05: made Table a subclass of DataFrame

--- matrix-methods/V05/Tables.py
+++ matrix-methods/V05/Tables.py
@@ -1,4 +1,4 @@
-## Compiled from Tables.ipynb on Tue Jun  7 20:40:00 2016
+## Compiled from Tables.ipynb on Wed Jun  8 12:03:41 2016
 
 ## In [1]:
 from __future__ import print_function
@@ -11,7 +11,31 @@ from IPython.core.magic import register_cell_magic
 import re
 
 ## In [2]:
-class Table(object):
+class Table(pd.DataFrame):
+    
+    _internal_names = pd.DataFrame._internal_names + ['filename']
+    _internal_names_set = set(_internal_names)
+    
+    _metadata = ['tablename','dsname']
+            
+    def __init__(self,*args,**kwargs):
+        dsname = kwargs.pop('dsname',None)
+        tablename = kwargs.pop('tablename',None)
+        filename = kwargs.pop('filename',None)
+        super(self.__class__,self).__init__(*args,**kwargs)
+        if dsname is not None:
+            self.dsname = dsname
+        if tablename is not None:
+            self.tablename = tablename
+        if filename is not None:
+            self.filename = filename
+        
+    @property
+    def _constructor(self):
+        return self.__class__
+
+## In [3]:
+class DataSet(object):    
     
     ROOT = 'data'
     DSNAME = None     # default data set name
@@ -19,7 +43,10 @@ class Table(object):
     #DSTYPE = 'cell'  # for CSV data provided via %%Table cell magic
     #DSTYPE = 'data'  # for dataframe data provided directly
     CELLDATA = {}     # csv text from %%Table magic cells, indexed by table name
-    DATAFRAMES = {}   # dataframes directly provided by client, indexed by table name
+    DATATABLES = {}   # dataframes directly provided by client, indexed by table name
+    
+    def __init__(self):
+        raise NotImplementedError("Cannot create instance of class '{}'".format(self.__class__.__name__))
     
     @classmethod
     def set_root(cls,root):
@@ -29,7 +56,7 @@ class Table(object):
     @classmethod
     def set_source(cls,dsname,dstype=None):
         if dstype is None:
-            dirname = cls.ROOT + '/' + dsname + '.d'
+            dirname = self.root + '/' + dsname + '.d'
             if os.path.exists(dirname):
                 dstype = 'dir'
             else:
@@ -38,69 +65,63 @@ class Table(object):
         cls.DSNAME = dsname
         cls.DSTYPE = dstype
         cls.CELLDATA = {}
-        cls.DATAFRAMES = {}
+        cls.DATATABLES = {}
         
     @classmethod
-    def set_data(cls,tablename,data):
-        assert cls.DSTYPE == 'data'
-        cls.DATAFRAMES[tablename] = data
-    
-    def __init__(self,tablename,dsname=None,columns=None,indexcol=None,optional=False,data=[]):
-        if dsname is None and self.DSNAME is not None:
-            dsname = self.DSNAME
-        self.dsname = dsname
-        self.tablename = tablename
-        self.prefix = None
-        self.filename = None
-        self.columns = columns
-        self.indexcol = indexcol
-        self.optional = optional
-        self.data = pd.DataFrame(data,columns=columns)
-        
-    def _file_name(self,prefix=None):
-        self.prefix = prefix
-        n = self.tablename
-        if prefix:
-            n = prefix + '/' + self.tablename
-        return 'data/' + self.ds_name + '.d/' + n + '.csv'
-        
-    def read(self,file_name=None,optional=None):
-        if optional is None:
-            optional = self.optional
-        if self.DSTYPE == 'dir':
-            if not file_name:
-                file_name = self._file_name()
-            self.filename = file_name
-            if optional:
-                if not os.path.exists(file_name):
-                    return self.data
-            stream = file(file_name,'r')
-        elif self.DSTYPE == 'cell':
-            if optional:
-                if self.tablename not in self.CELLDATA:
-                    return self.data
-            stream = StringIO.StringIO(self.CELLDATA[self.tablename])
-        elif self.DSTYPE == 'data':
-            if optional:
-                if self.tablename not in self.DATAFRAMES:
-                    return self.data
-            self.data = self.DATAFRAMES[self.tablename]
-            return self.data
-        else:
-            raise ValueError("Invalid DS Type: {}".format(self.DSTYPE))
+    def set_data(cls,tablename,table):
+        cls.DATATABLES[tablename] = table
+        if tablename in cls.CELLDATA:
+            del cls.CELLDATA[tablename]
             
-        try:
-            self.data = pd.read_csv(stream,usecols=self.columns,index_col=False,skipinitialspace=True)
-        except ValueError as err:
-            msg = err.args[0]
-            if msg.endswith('is not in list'):
-                c = msg.split("'")[1]
-                raise ValueError("'{}' is not in the set of columns in file '{}'".format(c,file_name))
-            if msg.startswith('Index') and msg.endswith('invalid'):
-                raise ValueError("Index column '{}' is not in the set of columns in file '{}'".format(self.index_col,file_name))
-            raise
-        stream.close()
-        return self.data
+    @classmethod
+    def set_cell(cls,tablename,celltext):
+        cls.CELLDATA[tablename] = celltext
+        if tablename in cls.DATATABLES:
+            del cls.DATATABLES[tablename]
+            
+    @classmethod
+    def _file_name(cls,tablename,prefix=None):
+        n = tablename
+        if prefix:
+            n = prefix + '/' + tablename
+        return cls.ROOT + '/' + cls.DSNAME + '.d/' + n + '.csv'
+    
+    @classmethod
+    def get_table(cls,tablename,optional=False,prefix=None,columns=None,extrasok=True):
+        stream = None
+        filename = None
+        t = None
+        if tablename in cls.DATATABLES:
+            t = cls.DATATABLES[tablename]
+        else:
+            if tablename in cls.CELLDATA:
+                stream = StringIO.StringIO(cls.CELLDATA[tablename])
+            else:
+                filename = cls._file_name(tablename,prefix=prefix)
+                if os.path.exists(filename):
+                    stream = file(filename,'r')
+            if stream is None:
+                if optional:
+                    d = pd.DataFrame(columns=columns)
+                else:
+                    raise ValueError("Table '{}' does not exist.".format(tablename))
+            else:
+                d = pd.read_csv(stream,index_col=None,skipinitialspace=True)
+            t = Table(d,dsname=cls.DSNAME,tablename=tablename,filename=filename)
+
+        if columns is None:
+            return t
+        prov = set(t.columns)
+        reqd = set(columns)
+        if reqd-prov:
+            raise ValueError("Columns missing for table '{}': {}. Required columns are: {}"
+                             .format(tablename,list(reqd-prov),columns))
+        if prov-reqd:
+            if not extrasok:
+                raise ValueError("Extra columns for table '{}': {}. Required columns are: '{}'"
+                                .format(tablename,list(prov-reqd),columns))
+            t = t[columns]
+        return t
     
     def write(self,ds_name=None,precision=None,index=False,prefix=None,makedir=False):
         if ds_name is None:
@@ -137,16 +158,16 @@ def signature(file_name):
     f.close()
     return m.hexdigest()
 
-## In [12]:
+## In [14]:
 @register_cell_magic('Table')
 def cell_table(line,cell):
     mo = re.match(r'\s*(\S+)\s*$',line)
     if not mo:
         raise ValueError('Usage: %%Table tablename')
-    table_name = mo.group(1)
-    global Table
-    Table.DSTYPE = 'cell'
-    Table.CELLDATA[table_name] = cell
+    tablename = mo.group(1)
+    global DataSet
+    DataSet.DSTYPE = 'cell'
+    DataSet.set_cell(tablename,cell)
 
 ## In [ ]:
 
