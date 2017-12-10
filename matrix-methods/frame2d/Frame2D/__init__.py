33e33377b0f6e25060e5ba532a4835a6cb322d36
Neal Holtz <neal.holtz@carleton.ca>
2017-12-10 13:59:09 -0500

    Much porting to Pyhton 3 and redoing import mechanism for Frame2D because thats broken in Python 3; still have probs generating large frames
    Signed-off-by: Neal Holtz <neal.holtz@carleton.ca>

--- matrix-methods/frame2d/Frame2D/__init__.py
+++ matrix-methods/frame2d/Frame2D/__init__.py
@@ -1,10 +1,26 @@
 from salib import extend, NBImporter
 
-Frame2D = True,False
+## Because of the idiotic Python 3 import rules, we have to add
+## this directory to the path, otherwise we cannot use notebook
+## pages both as modules and as scripts.
 
-from .Frame2D_Base import Frame2D, ResultSet
-
-from . import Frame2D_Input
-from . import Frame2D_Output
-from . import Frame2D_Display
-from . import Frame2D_SolveFirstOrder
+import sys
+try:
+    __path = __file__.split('/')[:-1]
+    __thisdir = '/'.join(__path)
+    if __thisdir not in sys.path:
+        sys.path = sys.path[:1] + [__thisdir] + sys.path[1:]
+except Exception as e:
+    print(__e)
+    
+import Nodes
+import NodeLoads
+import Members
+import MemberLoads
+import LoadSets
+from Frame2D_Base import Frame2D, ResultSet
+import Frame2D_Input
+import Frame2D_Output
+import Frame2D_Display
+import Frame2D_SolveFirstOrder
+import Tables
--- salib/__init__.py
+++ salib/__init__.py
@@ -1,6 +1,3 @@
-# empty file so salib can be a module
-
-from __future__ import print_function
 import sys
 import inspect
 import types
